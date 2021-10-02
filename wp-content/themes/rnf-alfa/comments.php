72f53d547ae4cc957f38d89e19af5d7e61148dc5
Taylor Smith <tsmith@tsmithcreative.com>
2021-10-02 19:51:09 +0000

    Fix comment form problem once and for all by making it just vanish

---
+++ wp-content/themes/rnf-alfa/comments.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since Twenty Seventeen 1.0
+ * @version 1.0
+ */
+
+// Yeah and apparently for all the times I've turned off discussions and
+// disabled comments, old posts still have this form... so empty output it is.
